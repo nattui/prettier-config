f4ef348f4e29e5d9fe13fe937fe39b3a06ff412c
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-28 23:42:50 -0700

    chore: bump version to 0.0.8 and update plugin imports
    - Updated version in package.json to 0.0.8.
    - Changed prettier plugin imports in index.ts to string format for consistency.
    - Removed type definitions for prettier-plugin-css-order and prettier-plugin-packagejson as they are no longer needed.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import { type Config } from "prettier"
-import prettierPluginCssOrder from "prettier-plugin-css-order"
-import prettierPluginPackagejson from "prettier-plugin-packagejson"
-import prettierPluginSortJson from "prettier-plugin-sort-json"
 
 const config: Config = {
   cssDeclarationSorterOrder: "alphabetical",
   jsonRecursiveSort: true,
   plugins: [
-    prettierPluginCssOrder,
-    prettierPluginPackagejson,
-    prettierPluginSortJson,
+    "prettier-plugin-css-order",
+    "prettier-plugin-packagejson",
+    "prettier-plugin-sort-json",
   ],
   semi: false,
 }
